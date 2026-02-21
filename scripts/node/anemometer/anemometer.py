0ed4f2476f8c23fc7a5e71f56efb2a41b0f40df2
Luke <lm20052022@gmail.com>
2026-02-21 16:36:33 -0500

    anemometer

--- scripts/node/anemometer/anemometer.py
+++ scripts/node/anemometer/anemometer.py
@@ -2,22 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 #GPIO pin where signal wire is connected
-ANEMOMETER_PIN = 11
+ANEMOMETER_PIN = 17
 
 #Calibration factor for OS-FS01 (anemometer model)
 MS_PER_HZ = 0.6667
 
-pulse_count = 0
-
-def count_pulse(channel):
-    global pulse_count
-    pulse_count += 1
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ANEMOMETER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#Count falling edges from sensor
-GPIO.add_event_detect(ANEMOMETER_PIN, GPIO.FALLING, callback=count_pulse)
+last_state = GPIO.input(ANEMOMETER_PIN)
 
 print("Measuring wind speed ... Press CTRL+C to stop")
 
@@ -26,16 +19,17 @@ try:
         pulse_count = 0
         start_time = time.time()
 
-        time.sleep(2) #measurement window (seconds)
-        
-        duration = time.time() - start_time
-        frequency = pulse_count / duration
+        while time.time() - start_time < 2:
+            state = GPIO.input(ANEMOMETER_PIN)
+            if state == 0 and last_state == 1:
+                pulse_count += 1
+            last_state = state
 
+        frequency = pulse_count / 2
         wind_speed_ms = frequency * MS_PER_HZ
         wind_speed_kmh = wind_speed_ms * 3.6
 
-        print(f"Pulses: {pulse_count}")
-        print(f"Wind Speed: {wind_speed_ms:.2f} m/s | {wind_speed_kmh:.2f}")
+        print(f"{wind_speed_ms:.2f} m/s | {wind_speed_kmh:.2f} km/h")
         print("")
 
 except KeyboardInterrupt:
