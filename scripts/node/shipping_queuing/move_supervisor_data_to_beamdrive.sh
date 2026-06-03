c6894310af8de861a0daf0ed18a3bd81f6e740c8
Pazmaz <162527327+Pazmazz@users.noreply.github.com>
2026-06-03 09:44:26 -0400

    REMOVED mounting usb section from move_supervisor_data_to_beamdrive.sh to

--- scripts/node/shipping_queuing/move_supervisor_data_to_beamdrive.sh
+++ scripts/node/shipping_queuing/move_supervisor_data_to_beamdrive.sh
@@ -6,13 +6,6 @@ MOUNT_POINT="/home/pi/usbmnt"
 
 mkdir -p "$MOUNT_POINT"
 
-if findmnt -rn "$MOUNT_POINT" >/dev/null 2>&1; then
-  echo "USB already mounted at $MOUNT_POINT"
-else
-  echo "Mounting USB..."
-  sudo mount /dev/sda1 "$MOUNT_POINT"
-fi
-
 echo "Copying data..."
 sudo cp -r "$DATA_DIR" "$MOUNT_POINT/"
 
